302b2984043fc64bcef82af016697a07b294d554
Oksana Buday <oksanabuday@gmail.com>
2017-06-24 22:04:47 +0000

    Initial

---
+++ js/index.js
@@ -0,0 +1,20 @@
+
+
+var a = Number(prompt('Введіть a '));
+var b = Number(prompt('Введіть b'));
+var c = Number(prompt('Введіть c'));
+
+function quartion (a,b,c){
+    var rusult;
+
+    if (a === 0){
+        result = 'Введіть а відмінне від 0';
+        return result;
+    }
+}
+function D (a, b, c){
+   return b*b - 4*a*c;
+}
+ var X = (a,b,c){
+     
+ }
